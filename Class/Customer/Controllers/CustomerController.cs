604fe439de98e43449fb5a52b29d80c3593ce63f
Ivan Meireles <ivanmds2@hotmail.com>
2022-12-08 20:59:16 -0300

    projeto inicial

---
+++ Class/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Customer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private static List<CustomerEntity> _customers = new List<CustomerEntity>();
+
+        [HttpGet]
+        public IEnumerable<CustomerEntity> Get()
+        {
+            return _customers;
+        }
+
+
+        [HttpPost]
+        public CustomerEntity Post(CustomerEntity customer)
+        {
+            _customers.Add(customer);
+            return customer;
+        }
+
+    }
+}
---
+++ Class/Customer/Models/CustomerEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Customer.Models
+{
+    public class CustomerEntity
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Document { get; set; }
+
+        public List<string> Phones { get; set; }
+        public List<string> Address { get; set; }
+    }
+}
