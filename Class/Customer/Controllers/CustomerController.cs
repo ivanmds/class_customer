3b33ff2918484a912f15ee45982dd09df9118a2b
Ivan Meireles da Silva <isilva@bankly.com.br>
2022-12-15 20:28:52 -0300

    aula 15/12/2022

--- Class/Customer/Controllers/CustomerController.cs
+++ Class/Customer/Controllers/CustomerController.cs
@@ -7,20 +7,57 @@ namespace Customer.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private static List<CustomerEntity> _customers = new List<CustomerEntity>();
+        private static List<CustomerEntity> database = new List<CustomerEntity>();
 
         [HttpGet]
         public IEnumerable<CustomerEntity> Get()
         {
-            return _customers;
+            return database;
+        }
+
+        [HttpGet("{id:guid}")]
+        public CustomerEntity Get(Guid id)
+        {
+            return database.Find(p => p.Id == id);
         }
 
 
         [HttpPost]
-        public CustomerEntity Post(CustomerEntity customer)
+        public IActionResult Post(CustomerEntity customer)
         {
-            _customers.Add(customer);
-            return customer;
+            var customerFound = database.Find(item => item.Document == customer.Document);
+
+            if(customerFound != null)
+            {
+                return BadRequest($"The document informed {customer.Document} already in used.");
+            }
+
+            if (customer.Name.Length < 6)
+            {
+                return BadRequest($"The customer name should be greater than 5 caracters.");
+            }
+
+            customer.SetId(Guid.NewGuid());
+            database.Add(customer);
+            return Ok(customer);
+        }
+
+
+        [HttpPut("{id:guid}")]
+        public IActionResult Put(Guid id, [FromBody] CustomerEntity customer)
+        {
+            var customerFound = database.Find(p => p.Id == id);
+
+            if(customerFound == null)
+            {
+                return NotFound($"The customer with id {id} not found.");
+            }
+
+            database.Remove(customerFound);
+            customer.SetId(id);
+            database.Add(customer);
+
+            return Ok(customerFound);
         }
 
     }
--- Class/Customer/Models/CustomerEntity.cs
+++ Class/Customer/Models/CustomerEntity.cs
@@ -2,11 +2,17 @@
 {
     public class CustomerEntity
     {
+        public Guid Id { get; private set; }
         public string Name { get; set; }
         public DateTime Birthdate { get; set; }
         public string Document { get; set; }
 
         public List<string> Phones { get; set; }
         public List<string> Address { get; set; }
+
+        public void SetId(Guid id)
+        {
+            Id = id;
+        }
     }
 }
